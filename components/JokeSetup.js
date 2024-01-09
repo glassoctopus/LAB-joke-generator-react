3be35045419e9d839ccda8a5527ff7a74d4eaefa
Thomas McMahon <glassoctopus@yahoo.com>
2024-01-09 11:27:37 -0600

    mvp

---
+++ components/JokeSetup.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import getJoke from '../api/jokeData';
+
+export default function JokeSetup() {
+  const [joke, setJoke] = useState({ setup: '', punchline: '' });
+  const [punchlineState, setPunchlineState] = useState('');
+
+  function getJokes() {
+    getJoke().then((newJoke) => {
+      console.log(newJoke);
+      setJoke(newJoke);
+      setPunchlineState('');
+    });
+  }
+
+  function deliverPunchline() {
+    setPunchlineState('Punchline delivered!');
+  }
+
+  return (
+    <div
+      className="text-center d-flex flex-column justify-content-center align-content-center"
+      style={{
+        height: '90vh',
+        padding: '30px',
+        maxWidth: '666px',
+        margin: '0 auto',
+      }}
+    >
+      <h1 id="joke">{joke.setup}</h1>
+      {console.log(punchlineState)}
+      <button type="button" onClick={punchlineState ? getJokes : deliverPunchline}>
+        {punchlineState ? 'Get a Joke' : 'Get Punchline'}
+      </button>
+      {punchlineState && <h1 id="punchline">{joke.delivery}</h1>}
+    </div>
+  );
+}
